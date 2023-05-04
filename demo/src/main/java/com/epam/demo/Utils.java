e73749cc671d1ef4cabadbb4f394d8ad4ae168a7
Dzmitry Tsybulski <dzmitrytsybulski@gmail.com>
2023-05-04 13:16:30 +0300

    Create Stage 2 Module #1. Task #2. Build Tools. Maven

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,22 @@
 package com.epam.demo;
 
+
+
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        try {
+            for (String s : args) {
+                if (!StringUtils.isPositiveNumber(s)) {
+                    return false;
+                }
+            }
+        } catch (NullPointerException e) {
+            return false;
+        }
+        return true;
     }
 }
